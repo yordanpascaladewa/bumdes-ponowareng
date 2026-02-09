19e7197f98d6ce5c88808e76bd15919be0e18c4d
Yordan Pascaladewa <yordanpascaladewaa@gmail.com>
2026-02-09 15:03:29 +0700

    p

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+// Pastikan baris ini ada! Ini yang bikin web jadi berwarna
+import '@/styles/globals.css'; 
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
